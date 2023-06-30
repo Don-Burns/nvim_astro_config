b4c7e0ef7d822f675e01005a40571897b00260e1
Donal Burns <donal.w.burns@hotmail.com>
2023-06-30 23:12:32 +0100

    Reformat mason lsps

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,18 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "lua_ls", "ruff_lsp", "rust_analyzer"
+        -- lua
+        "lua_ls",
+        -- python
+        "ruff_lsp",
+        -- rust
+        "rust_analyzer",
+        -- docker
+        -- "dockerls",
+        -- scala
+        -- "metals",
+        -- sql
+        -- "sqlls"
       })
     end,
   },
